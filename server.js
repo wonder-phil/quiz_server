aee956cded600c89f2f99a6dd28f561323fb5b69
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2022-04-25 15:53:27 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+
+
+// Started from: ...
+
+// server.js
+const express = require('express');
+// Define Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+// Serve Static Assets
+app.use(express.static('images'));
+//app.use(express.static('questions'));
+// Virtual Path Prefix '/static'
+app.use('/static', express.static('images'))
+//app.use('/static',  express.static('questions'));
+app.use('/questions', (req, res) => {
+    res.header("Content-Type",'application/json');
+    res.sendFile("c:\\users\\pgb15001\\t\\questionServe\\questions\\q_list1.json");
+  });
+app.get('/test', (req, res) => res.json({ "answer": 42 }));
+
+app.listen(PORT, () => {
+    console.log('Server connected at:', PORT);
+});
